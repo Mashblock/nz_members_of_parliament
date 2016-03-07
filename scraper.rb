5d43a7ab1882c1baa804b9248f57199e34e9e22f
Cameron Prebble <cameron.prebble@abletech.co.nz>
2016-03-07 08:19:00 +0100

    scape pages

--- scraper.rb
+++ scraper.rb
@@ -1,13 +1,33 @@
 # This is a template for a Ruby scraper on morph.io (https://morph.io)
 # including some code snippets below that you should find helpful
 
-# require 'scraperwiki'
-# require 'mechanize'
+require 'scraperwiki'
+require 'mechanize'
 #
-# agent = Mechanize.new
+agent = Mechanize.new
 #
 # # Read in a page
-# page = agent.get("http://foo.com")
+page = agent.get("http://parliament.govt.nz/en-nz/mpp/mps/current?Criteria.ViewAll=1")
+links = page.links_with(href: /\/en-nz\/mpp\/mps\/current\/51MP/)
+links.each do |link|
+  mp_page = link.click
+  seat, party = mp_page.at('div.copy h2').text.split(',').map(&:strip)
+  email = mp_page.links_with(href:/mailto\:/).first.href.gsub('mailto:', '') rescue ''
+  twitter = mp_page.links_with(href:/twitter.com/).first.href rescue ''
+  facebook = mp_page.links_with(href:/facebook.com/).first.href rescue ''
+
+  details = {
+    name: mp_page.at('div.copy h1').text,
+    seat: seat,
+    party: party,
+    email: email,
+    twitter: twitter,
+    facebook: facebook
+  }
+
+  ScraperWiki.save_sqlite([:name], details)
+end
+
 #
 # # Find somehing on the page using css selectors
 # p page.at('div.content')
